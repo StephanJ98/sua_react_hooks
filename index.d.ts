591bed841536d1449d20dd1c7f928ea4a0ea65ae
StephanJ98 <diego.jeandon@gmail.com>
2023-06-05 18:49:23 +0200

    :zap: Add Typescript support

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+export declare const useToggleState: () => {
+    state: boolean;
+    toggle: () => void;
+};
+export declare const useWindowSize: () => {
+    width: null;
+    height: null;
+};
--- index.ts
+++ index.ts
@@ -6,17 +6,22 @@ export const useToggleState = (): { state: boolean, toggle: () => void } => {
     return { state, toggle }
 }
 
-export const useWindowSize = (): { size: [number, number] } => {
+export const useWindowSize = () => {
     const [size, setSize] = React.useState({
         width: null,
         height: null
     })
 
     React.useLayoutEffect(() => {
+
+        const width: any = window.innerWidth
+        const height: any = window.innerHeight
+
+
         const handleResize = () => {
             setSize({
-                width: window.innerWidth,
-                height: window.innerHeight
+                width,
+                height
             })
         };
 
